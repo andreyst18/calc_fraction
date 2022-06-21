5f94eaaf3fe26764b871566caa60aea2256954cb
Andrey <andrejmax@yandex.ru>
2022-06-21 18:56:19 +0400

    first commit

---
+++ src/components/Calculator.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="section calculator">
+    <div class="calculator__input-fractions input-fractions">
+      <Fraction></Fraction>
+      <Fraction></Fraction>
+    </div>
+    <AddFractionBtn></AddFractionBtn>
+    <SaveCalcBtn></SaveCalcBtn>
+  </section>
+</template>
+
+<script>
+import Fraction from './Fraction.vue'
+
+export default {
+  components: {
+    Fraction
+  },
+  data() {
+    return {
+      fractions: [],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .input-fractions {
+    display: flex;
+  }
+</style>
---
+++ src/components/Fraction.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="fraction">
+    <input class="fraction__item" type="text" v-model="numerator">
+    <div class="fraction__line"></div>
+    <input class="fraction__item" type="text" v-model="denominator">
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        numerator: '',
+        denominator: ''
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+.fraction {
+  &__line {
+    width: 59px;
+    height: 1px;
+    background: #383129;
+;
+  }
+}
+
+</style>
