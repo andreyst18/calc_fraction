96986baf2f3f34767cb9d9a039d534bccb8b80f3
Andrey <andrejmax@yandex.ru>
2022-06-23 20:06:32 +0400

    upg calculator section

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -6,9 +6,9 @@
       <Fraction @add-fraction="addFraction"></Fraction>
       <EqualSymbol class="input-fractions__equal"></EqualSymbol>
       <div class="input-fractions__result result">
-        <div class="result__whole">{{ getResult }}</div>
+        <div class="result__whole">{{ getWholePartResult[0] }}</div>
         <div class="result__fractional fractional">
-          <div class="fractional__item"></div>
+          <div class="fractional__item">{{ getWholePartResult[1] }}</div>
           <div class="fractional__item"></div>
         </div>
       </div>
@@ -19,54 +19,76 @@
 </template>
 
 <script>
-import Fraction from './Fraction.vue'
-import PlusSymbol from './PlusSymbol.vue'
-import EqualSymbol from './EqualSymbol.vue'
+import Fraction from "./Fraction.vue";
+import PlusSymbol from "./PlusSymbol.vue";
+import EqualSymbol from "./EqualSymbol.vue";
 
 export default {
   components: {
     Fraction,
     PlusSymbol,
-    EqualSymbol
+    EqualSymbol,
   },
   data() {
     return {
       fractions: [],
-      result: '?',
-      fractionsQuantity: 2
+      result: "?",
+      fractionsQuantity: 2,
     };
   },
   methods: {
     addFraction(numerator, denominator) {
       this.fractions.push([+numerator, +denominator]);
-      console.log(this.fractions);
 
-      if(this.checkResultConditions) {
-        console.log(this.fractions[0]);
+      if (this.checkResultConditions) {
         this.getResult;
       }
-      
     },
     checkResultConditions() {
       if (this.fractions.length === this.fractionsQuantity) {
         return true;
       }
       return false;
-    }
-    
+    },
   },
   computed: {
-    getResult() {
+    getWholePartResult() {
       if (!this.checkResultConditions()) {
-        return  '?';
+        return "?";
       }
-      let result = 0;
-      this.fractions.forEach( el => {
-        result += el[1]
+
+      const commonMultiple = this.getCommonMultiple;
+      const fractionsModified = [];
+      let sum = 0;
+      let wholeResult = 0;
+      let numeratorResult = 0;
+      let denominatorResult = 0;
+      for (let i = 0; i < this.fractions.length; i++) {
+        const multiplier = commonMultiple / this.fractions[i][1];
+        const fractionModified = this.fractions[i].map(
+          (el) => (el *= multiplier)
+        );
+        fractionsModified.push(fractionModified);
+        sum += fractionModified[0];
+      }
+      wholeResult = Math.floor(sum / fractionsModified[0][1]);
+      //здесь будет расчет - числитель и знаменатель
+      return [wholeResult, sum % fractionsModified[0][1]];
+    },
+    // getNumeratorResult() {
+
+    // },
+    // getDenominatorResult() {
+
+    // },
+    getCommonMultiple() {
+      let result = 1;
+      this.fractions.forEach((el) => {
+        result *= el[1];
       });
       return result;
-    }
-  }
+    },
+  },
 };
 </script>
 
