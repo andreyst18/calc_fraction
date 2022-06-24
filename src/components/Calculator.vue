5183f0679a69e33d71d70ce1847e3b3605041259
Andrey <andrejmax@yandex.ru>
2022-06-24 20:52:28 +0400

    upd calculator section

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -4,17 +4,23 @@
       <Fraction @add-fraction="addFraction"></Fraction>
       <PlusSymbol class="input-fractions__plus"></PlusSymbol>
       <Fraction @add-fraction="addFraction"></Fraction>
+      <ul>
+        <li v-for="(item, index) in additionalFractions" :key="index">
+          <PlusSymbol class="input-fractions__plus"></PlusSymbol>
+          <Fraction @add-fraction="addFraction"></Fraction>
+        </li>
+      </ul>
       <EqualSymbol class="input-fractions__equal"></EqualSymbol>
       <div class="input-fractions__result result">
-        <div class="result__whole">{{ getWholePartResult[0] }}</div>
+        <div class="result__whole">{{ getResult[0] }}</div>
         <div class="result__fractional fractional">
-          <div class="fractional__item">{{ getWholePartResult[1] }}</div>
-          <div class="fractional__item"></div>
+          <div class="fractional__item">{{ getResult[1] || '' }}</div>
+          <div class="fractional__item">{{ getResult[2] || '' }}</div>
         </div>
       </div>
     </div>
-    <!-- <AddFractionBtn></AddFractionBtn>
-    <SaveCalcBtn></SaveCalcBtn> -->
+    <AddFractionBtn @add-fraction="addNewFractionForm()"></AddFractionBtn>
+    <!-- <SaveCalcBtn></SaveCalcBtn> -->
   </section>
 </template>
 
@@ -22,16 +28,19 @@
 import Fraction from "./Fraction.vue";
 import PlusSymbol from "./PlusSymbol.vue";
 import EqualSymbol from "./EqualSymbol.vue";
+import AddFractionBtn from "./AddFractionBtn.vue"
 
 export default {
   components: {
     Fraction,
     PlusSymbol,
     EqualSymbol,
+    AddFractionBtn
   },
   data() {
     return {
       fractions: [],
+      additionalFractions: [],
       result: "?",
       fractionsQuantity: 2,
     };
@@ -50,9 +59,16 @@ export default {
       }
       return false;
     },
+    addNewFractionForm() {
+      if (this.fractionsQuantity < 5) {
+        this.additionalFractions.push(1);
+        this.fractionsQuantity++
+      }
+      console.log(this.fractions)
+    }
   },
   computed: {
-    getWholePartResult() {
+    getResult() {
       if (!this.checkResultConditions()) {
         return "?";
       }
@@ -60,9 +76,6 @@ export default {
       const commonMultiple = this.getCommonMultiple;
       const fractionsModified = [];
       let sum = 0;
-      let wholeResult = 0;
-      let numeratorResult = 0;
-      let denominatorResult = 0;
       for (let i = 0; i < this.fractions.length; i++) {
         const multiplier = commonMultiple / this.fractions[i][1];
         const fractionModified = this.fractions[i].map(
@@ -71,16 +84,26 @@ export default {
         fractionsModified.push(fractionModified);
         sum += fractionModified[0];
       }
-      wholeResult = Math.floor(sum / fractionsModified[0][1]);
-      //здесь будет расчет - числитель и знаменатель
-      return [wholeResult, sum % fractionsModified[0][1]];
+      let wholeResult = Math.floor(sum / fractionsModified[0][1]);
+      let numeratorResult = sum % fractionsModified[0][1];
+      let denominatorResult = fractionsModified[0][1];
+
+      //Попытка сократить дробь
+      let greatestCommonDivisor = 1;
+      for (let i = 2; i <= numeratorResult; i++) {
+        if (numeratorResult % i === 0 && denominatorResult % i === 0) {
+          greatestCommonDivisor = i;
+        }
+        console.log(greatestCommonDivisor)
+      }
+
+      denominatorResult = numeratorResult === 0 ? 0 : denominatorResult;
+      
+      return [wholeResult, 
+              numeratorResult / greatestCommonDivisor,
+              denominatorResult / greatestCommonDivisor
+            ];
     },
-    // getNumeratorResult() {
-
-    // },
-    // getDenominatorResult() {
-
-    // },
     getCommonMultiple() {
       let result = 1;
       this.fractions.forEach((el) => {
@@ -88,6 +111,7 @@ export default {
       });
       return result;
     },
+    
   },
 };
 </script>
